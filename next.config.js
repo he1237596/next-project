82a503c6a1e064029da1567baeb404df98453f8f
Chris <raoxc@mogulinker.com>
2020-12-18 01:22:39 +0800

    修改next.config,js启用css模块化成功，但antd样式又丢失

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
  * @Author: Chris
  * @Date: 2020-12-18 01:05:03
  * @LastEditors: Chris
- * @LastEditTime: 2020-12-18 01:18:04
+ * @LastEditTime: 2020-12-18 01:21:23
  * @Descripttion: **
  */
 const withLess = require('@zeit/next-less')
 const withCss = require('@zeit/next-css')
 
-module.exports=withLess(withCss());
+module.exports=withLess(withCss({ cssModules: true }));
