3630fe2c814a6c5418a19a690d0072457e5259d1
Chris <raoxc@mogulinker.com>
2020-12-18 01:20:50 +0800

    less配置成功，但是css模块化失效

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
  * @Author: Chris
  * @Date: 2020-12-18 01:05:03
  * @LastEditors: Chris
- * @LastEditTime: 2020-12-18 01:13:54
+ * @LastEditTime: 2020-12-18 01:18:04
  * @Descripttion: **
  */
 const withLess = require('@zeit/next-less')
 const withCss = require('@zeit/next-css')
 
-module.exports=withLess(withCss({ cssModules: true }));
+module.exports=withLess(withCss());
