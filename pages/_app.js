9cba33447d6a3226e7e84a77c6a780c204b4fb80
Chris <raoxc@mogulinker.com>
2020-12-18 00:54:38 +0800

    引入antd 配置babelrc按需加载 全局引入样式

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
+/*
+ * @Author: Chris
+ * @Date: 2020-12-18 00:23:28
+ * @LastEditors: Chris
+ * @LastEditTime: 2020-12-18 00:41:47
+ * @Descripttion: **
+ */
 import '../styles/globals.css'
+import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@
  * @Author: Chris
  * @Date: 2020-12-18 00:23:28
  * @LastEditors: Chris
- * @LastEditTime: 2020-12-18 00:25:29
+ * @LastEditTime: 2020-12-18 00:39:13
  * @Descripttion: **
  */
 import Head from 'next/head'
+import {Button} from 'antd'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Index</h1>
+      <Button type='primary'>按钮</Button>
     </div>
   )
 }
